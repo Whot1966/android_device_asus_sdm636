4d71cc0d219f4990720746d9cd265e66d04658e3
Whot1966 <Whot1966@users.noreply.github.com>
2023-04-30 13:35:42 +0700

    X00TD: init: Rewrite to pass safetynet shenanigans by default * Use stock props from asus 16.2017.2009.087-20200826 * Inspired from https://github.com/AOSPA/android_device_xiaomi_alioth/blob/topaz/init/init_alioth.cpp
    Co-authored-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>

--- init/init_X00TD.cpp
+++ init/init_X00TD.cpp
@@ -1,57 +1,38 @@
 /*
-   Copyright (c) 2016, The CyanogenMod Project
-   Copyright (c) 2019, The LineageOS Project
-
-   Redistribution and use in source and binary forms, with or without
-   modification, are permitted provided that the following conditions are
-   met:
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above
-      copyright notice, this list of conditions and the following
-      disclaimer in the documentation and/or other materials provided
-      with the distribution.
-    * Neither the name of The Linux Foundation nor the names of its
-      contributors may be used to endorse or promote products derived
-      from this software without specific prior written permission.
-
-   THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
-   WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
-   ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
-   BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
-   BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-   WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-   OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
-   IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Copyright (C) 2022 PixelExperience
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
-#include <cstdlib>
 #include <fstream>
-#include <string.h>
-#include <sys/sysinfo.h>
-#include <unistd.h>
+#include <vector>
 
+#include <android-base/logging.h>
 #include <android-base/properties.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
-
-#include "vendor_init.h"
-#include "property_service.h"
+#include <sys/sysinfo.h>
 
 using android::base::GetProperty;
-using std::string;
 
-void property_override(string prop, string value)
-{
-    auto pi = (prop_info*) __system_property_find(prop.c_str());
+void property_override(char const prop[], char const value[], bool add = true) {
+    prop_info* pi;
 
-    if (pi != nullptr)
-        __system_property_update(pi, value.c_str(), value.size());
-    else
-        __system_property_add(prop.c_str(), prop.size(), value.c_str(), value.size());
+    pi = (prop_info*)__system_property_find(prop);
+    if (pi)
+        __system_property_update(pi, value, strlen(value));
+    else if (add)
+        __system_property_add(prop, strlen(prop), value, strlen(value));
 }
 
 void set_avoid_gfxaccel_config() {
@@ -78,8 +59,24 @@ void NFC_check()
         property_override("ro.hq.support.nfc", "0");
 }
 
-void vendor_load_properties()
-{
+void load_vendor_props() {
+    property_override("bluetooth.device.default_name", "Zenfone Max Pro M1");
+    property_override("ro.product.brand", "asus");
+    property_override("ro.product.manufacturer", "asus");
+    property_override("ro.product.device", "ASUS_X00TD");
+    property_override("ro.vendor.product.device", "ASUS_X00TD");
+    property_override("ro.product.name", "WW_X00TD");
+    property_override("ro.vendor.product.name", "WW_X00TD");
+    property_override("ro.build.fingerprint", "Android/sdm660_64/sdm660_64:9/PKQ1/16.2017.2009.087-20200826:user/release-keys");
+    property_override("ro.vendor.build.fingerprint", "Android/sdm660_64/sdm660_64:9/PKQ1/16.2017.2009.087-20200826:user/release-keys");
+    property_override("ro.build.description", "sdm660_64-user 9 PKQ1 43 release-keys");
+    property_override("vendor.usb.product_string", "Zenfone Max Pro M1");
+}
+
+void vendor_load_properties() {
+    if (access("/system/bin/recovery", F_OK) != 0) {
+        load_vendor_props();
+    }
     set_avoid_gfxaccel_config();
     NFC_check();
 }
