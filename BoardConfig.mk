PixysOS-Devices/device_asus_X00TD

X00TD: Include vendor/debugfs.config
This disables the following kernel configs:
* CONFIG_DEBUG_FS
* CONFIG_PAGE_OWNER

Change-Id: Idbf74e3dee4dccff4708462271e79374e28449ae

BoardConfig.mk
@@ -20,7 +20,7 @@ TARGET_BOOTLOADER_BOARD_NAME := sdm636
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
 # Kernel
-TARGET_KERNEL_CONFIG := vendor/X00TD_defconfig
+TARGET_KERNEL_CONFIG := vendor/X00TD_defconfig vendor/debugfs.config
 
 # Inherit the proprietary files
 include vendor/asus/X00TD/BoardConfigVendor.mk

Whot1966/android_device_asus_sdm636

BoardConfig.mk
@@ -66,7 +66,7 @@ BOARD_KERNEL_IMAGE_NAME  := Image.gz-dtb
 
 TARGET_KERNEL_SOURCE := kernel/asus/sdm660
 TARGET_KERNEL_VERSION := 4.19
-TARGET_KERNEL_CONFIG := asus/X00TD_defconfig
+TARGET_KERNEL_CONFIG := asus/X00TD_defconfig vendor/debugfs.config
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
     HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"