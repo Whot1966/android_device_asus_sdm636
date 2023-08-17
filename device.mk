5c2c51d391dddab3f32588391f4043ba8f71d75e
Whot1966 <Whot1966@users.noreply.github.com>
2023-08-17 00:22:36 +0000

    Revert "X00TD: Create power init script"
    This reverts commit 169714b2df5e183556b63955755a8a7c374397d0.

--- device.mk
+++ device.mk
@@ -382,7 +382,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fstab.qcom \
     init.qcom.asus.rc \
-    init.qcom.power.rc \
     init.qcom.rc \
     init.parts.rc \
     init.recovery.qcom.rc \
