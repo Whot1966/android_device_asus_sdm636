c320f5be495ba1e74fc9a9a2a9519887152528aa
Whot1966 <Whot1966@users.noreply.github.com>
2023-09-21 20:31:51 +0700

    [WIP] X00TD: Retrofit dynamic partitions

--- device.mk
+++ device.mk
@@ -160,6 +160,15 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor \
     android.hardware.drm-service.clearkey
 
+# Dynamic Partitions
+PRODUCT_BUILD_SUPER_PARTITION := false
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_RETROFIT_DYNAMIC_PARTITIONS := true
+
+# Fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service-X00TD
