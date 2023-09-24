9be4735e52206f33a9e252d31567e539b5185959
Whot1966 <Whot1966@users.noreply.github.com>
2023-09-24 19:56:16 +0700

    WIP fixup!

--- Android.mk
+++ Android.mk
@@ -10,19 +10,6 @@ ifeq ($(TARGET_DEVICE),X00TD)
   subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
   $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
 
-LPFLASH := $(HOST_OUT_EXECUTABLES)/lpflash$(HOST_EXECUTABLE_SUFFIX)
-INSTALLED_SUPERIMAGE_DUMMY_TARGET := $(PRODUCT_OUT)/super_dummy.img
-
-$(INSTALLED_SUPERIMAGE_DUMMY_TARGET): $(PRODUCT_OUT)/super_empty.img $(LPFLASH)
-	$(call pretty,"Target dummy super image: $@")
-	$(hide) touch $@
-	$(hide) $(LPFLASH) $@ $(PRODUCT_OUT)/super_empty.img
-
-.PHONY: super_dummyimage
-super_dummyimage: $(INSTALLED_SUPERIMAGE_DUMMY_TARGET)
-
-INSTALLED_RADIOIMAGE_TARGET += $(INSTALLED_SUPERIMAGE_DUMMY_TARGET)
-
 include $(CLEAR_VARS)
 
 # Create symbolic links for msadp
